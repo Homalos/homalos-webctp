d450642b77de8dec4e2c6c660dd3a9ef971dbaf8
Lumosylva <donnymoving@gmail.com>
2025-12-03 13:56:36 +0800

    feat(log): 添加日志工具模块
    - 创建日志工具初始化文件
    - 实现基础日志记录功能
    - 配置日志文件头信息
    - 设置项目相关元数据
    - 定义日志工具类结构
    - 添加编码和作者信息

---
+++ utils/log/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: webctp
+@FileName   : __init__.py.py
+@Date       : 2025/12/3 13:54
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
---
+++ utils/log/logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: webctp
+@FileName   : logger.py
+@Date       : 2025/12/3 13:55
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 日志工具类
+"""
