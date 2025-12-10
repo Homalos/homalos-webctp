7fa1b919722209fc8238f1586679ab248bd8afa5
Lumosylva <donnymoving@gmail.com>
2025-12-10 09:20:10 +0800

    chore(md_client): 添加文件头注释
    - 添加标准的Python文件头注释，包含项目信息、文件名、日期、作者等元数据
    - 补充编码声明和脚本解释器指令
    - 添加模块描述信息

--- src/clients/md_client.py
+++ src/clients/md_client.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-webctp
+@FileName   : md_client.py
+@Date       : 2025/12/3 13:40
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: md_client
+"""
 import logging
 import time
 import uuid
