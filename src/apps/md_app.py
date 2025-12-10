e0b41a6b10905eff795f45fb2c0ff8856f4a9a8c
Lumosylva <donnymoving@gmail.com>
2025-12-10 09:20:14 +0800

    docs(md_app): 添加文件头部注释信息
    - 添加Python文件标准头部注释
    - 包含项目名称、文件名、创建日期、作者信息
    - 添加作者邮箱和开发环境信息
    - 添加模块功能描述

--- src/apps/md_app.py
+++ src/apps/md_app.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-webctp
+@FileName   : md_app.py
+@Date       : 2025/12/3 13:20
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: md_app
+"""
 from fastapi import FastAPI, WebSocket
 from ..services.connection import MdConnection
 
