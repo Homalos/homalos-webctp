e38306abcd1eaf628c746db88a3e5503970372cf
Lumosylva <donnymoving@gmail.com>
2025-12-10 09:20:30 +0800

    docs(call_errors): 添加文件头注释
    - 添加Python文件头注释，包含项目名称、文件名、日期等信息
    - 添加作者信息和联系方式
    - 添加文件描述说明
    - 保持原有导入语句不变

--- src/constants/call_errors.py
+++ src/constants/call_errors.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-webctp
+@FileName   : call_errors.py
+@Date       : 2025/12/3 13:58
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: call_errors
+"""
 from typing import Any
 
 
