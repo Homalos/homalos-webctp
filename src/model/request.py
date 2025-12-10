27b7d5588f496b4a5b782585d0cc4b1b6d0c928c
Lumosylva <donnymoving@gmail.com>
2025-12-10 09:20:05 +0800

    docs(model): 为request模块添加文件头注释
    - 添加Python环境声明和编码声明
    - 添加项目名称、文件名、日期等基础信息
    - 包含作者信息和联系方式
    - 添加模块描述说明

--- src/model/request.py
+++ src/model/request.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-webctp
+@FileName   : request.py
+@Date       : 2025/12/3 14:00
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: request
+"""
 from pydantic import BaseModel, Field
 
 
