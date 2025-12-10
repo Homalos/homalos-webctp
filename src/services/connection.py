4497f8ec8d9a15ada811e60af1d6677f4c26ace1
Lumosylva <donnymoving@gmail.com>
2025-12-10 09:15:42 +0800

    chore(connection): 添加文件头注释
    - 在 connection.py 文件开头添加标准的 Python 文件头注释
    - 包含项目名称、文件名、日期、作者信息等元数据
    - 保持代码格式规范性和可维护性

--- src/services/connection.py
+++ src/services/connection.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-webctp
+@FileName   : connection.py
+@Date       : 2025/12/3 14:18
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: connection
+"""
 import abc
 import anyio
 import json
