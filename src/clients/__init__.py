7beeba6ff9f74b8d3ec833b9d577224848f0c378
Lumosylva <donnymoving@gmail.com>
2026-02-01 00:00:25 +0800

    feat(core): 初始化客户端和服务工具模块
    - 添加了 CTP 行情和交易客户端接口
    - 集成了基础配置管理功能
    - 添加了对象辅助工具类
    - 引入了数学计算辅助函数
    - 配置了统一的日志记录器
    - 定义了模块公共接口导出列表

---
+++ src/clients/__init__.py
@@ -0,0 +1,5 @@
+from .md_client import MdClient as CTPMdClient
+from .td_client import TdClient as CTPTdClient
+
+__all__ = ['CTPMdClient', 'CTPTdClient', 'MdClient', 'TdClient']
+
---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+from .config import GlobalConfig
+from .ctp_object_helper import CTPObjectHelper
+from .log.logger import logger
+from .math_helper import MathHelper
+
+__all__ = ['CTPObjectHelper', 'GlobalConfig', 'MathHelper', 'logger']
+
